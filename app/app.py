127930606eac25564fd33596ca1ddf76c0e62083
Anna Kioko <annakioko00@gmail.com>
2024-06-13 13:23:02 +0300

    models updated

---
+++ app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, make_response, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token, create_refresh_token
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from model import db, User, Product, Order, Cart, CartItem
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+load_dotenv()
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+
+migrate = Migrate(app, db)
+db.init_app(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+api = Api(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
